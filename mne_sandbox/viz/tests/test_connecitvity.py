91c4d7603c93e8afd29affc061108121ba04c527
Martin Billinger <flkazemakase@gmail.com>
2016-04-28 10:30:50 +0200

    Added tests for improved coverage of viz

--- mne_sandbox/viz/tests/test_connecitvity.py
+++ mne_sandbox/viz/tests/test_connecitvity.py
@@ -4,10 +4,12 @@
 
 import numpy as np
 from numpy.testing import assert_array_equal
+from nose.tools import assert_raises, assert_equal
 
 from mne_sandbox.viz import (plot_connectivity_circle,
                              plot_connectivity_matrix,
                              plot_connectivity_inoutcircles)
+from mne_sandbox.viz.connectivity import _plot_connectivity_matrix_nodename
 
 # Set our plotters to test mode
 import matplotlib
@@ -35,10 +37,27 @@ def test_plot_connectivity_matrix():
     con = np.random.RandomState(42).rand(5, 5)
     con0 = con.copy()
 
-    plot_connectivity_matrix(con, label_names)
+    assert_raises(ValueError, plot_connectivity_matrix,
+                  con=np.empty((2, 2, 2)), node_names=label_names)
+    assert_raises(ValueError, plot_connectivity_matrix,
+                  con=np.empty((1, 2)), node_names=label_names)
+
+    plot_connectivity_matrix(con, label_names, colormap='jet', title='Test')
 
     # check that function does not change arguments
     assert_array_equal(con, con0)
+
+    # test status bar text
+    labels = ['a', 'b', 'c', 'd']
+    con = np.empty((8, 8))
+    con[:] = np.nan
+    con[2:-2, 2:-2] = np.arange(16).reshape(4, 4)
+
+    str1 = _plot_connectivity_matrix_nodename(1, 1, con, labels)
+    str2 = _plot_connectivity_matrix_nodename(2, 3, con, labels)
+
+    assert_equal(str1, '')
+    assert_equal(str2, 'a --> b: 4')
 
 
 def test_plot_connectivity_inoutcircles():
@@ -50,7 +69,13 @@ def test_plot_connectivity_inoutcircles():
     con = np.random.RandomState(42).rand(5, 5)
     con0 = con.copy()
 
-    plot_connectivity_inoutcircles(con, 'bankssts-rh', label_names)
+    assert_raises(ValueError, plot_connectivity_inoutcircles, con, 'n/a',
+                  label_names)
+    assert_raises(ValueError, plot_connectivity_inoutcircles, con, 99,
+                  label_names)
+
+    plot_connectivity_inoutcircles(con, 'bankssts-rh', label_names,
+                                   title='Test')
 
     # check that function does not change arguments
     assert_array_equal(con, con0)
